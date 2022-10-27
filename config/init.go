25514262bfa9f018f1108f86cfe47b3d45968c8b
lushuncheng <i@shuncheng.lu>
2022-10-27 08:05:50 +0800

    init log

--- config/init.go
+++ config/init.go
@@ -31,30 +31,33 @@ const (
 
 func init() {
 	var c string
-	flag.StringVar(&c, "c", "config.json", "配置文件，默认为当前路径下的config.json")
+	flag.StringVar(&c, "c", "config.json", "config file，default is config.json in current directory")
 	flag.Parse()
-
+	if len(c) == 0 {
+		c = "config.json"
+	}
 	if strings.Index(c, "/") != 0 {
 		p, err := os.Getwd()
 		if nil != err {
-			panic(err)
+			fmt.Printf("read config file with error：%+v", err)
+			os.Exit(1)
 		}
 		c = path.Join(p, c)
 	}
 	// load json config file
 	jsonFile, err := os.OpenFile(c, os.O_RDONLY, 0755)
 	if nil != err {
-		fmt.Printf("读取配置文件错误：%+v", err)
+		fmt.Printf("read config file with error：%+v", err)
 		os.Exit(1)
 	}
 	jsonData, err := io.ReadAll(jsonFile)
 	if nil != err {
-		fmt.Printf("读取配置文件错误：%+v", err)
+		fmt.Printf("read config file with error：%+v", err)
 		os.Exit(1)
 	}
 	err = json.Unmarshal(jsonData, Config)
 	if nil != err {
-		fmt.Printf("解析配置文件错误：%+v", err)
+		fmt.Printf("parse config with error：%+v", err)
 		os.Exit(1)
 	}
 }
