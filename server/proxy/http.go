02eac0934378e829e1d2d1c56b6013369c5e145f
lushuncheng <i@shuncheng.lu>
2022-11-02 23:39:33 +0800

    tmp

--- server/proxy/http.go
+++ server/proxy/http.go
@@ -3,6 +3,7 @@ package proxy
 import (
 	"io"
 	"net"
+	"net/http"
 
 	"proxy/server/common"
 	"proxy/utils/context"
@@ -19,5 +20,10 @@ func (s *HttpServer) Start(l net.Listener) {
 
 }
 func (s *HttpServer) Handshake(ctx *context.Context, conn net.Conn) (io.ReadWriter, *common.TargetAddr, error) {
+	req, _ := ctx.Get("request")
+	request, _ := req.(*http.Request)
+	if request.Method == http.MethodConnect {
+
+	}
 	return nil, nil, nil
 }
--- server/proxy/tls.go
+++ server/proxy/tls.go
@@ -2,10 +2,12 @@ package proxy
 
 import (
 	"crypto/tls"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"net"
 
+	"github.com/pkg/errors"
 	"proxy/config"
 	"proxy/server/common"
 	"proxy/utils/context"
@@ -16,12 +18,41 @@ type TlsRemote struct {
 
 func (r *TlsRemote) Handshake(ctx *context.Context, target *common.TargetAddr) (io.ReadWriter, error) {
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", config.Config.Out.RemoteAddr, "443"))
+	if nil != err {
+		return nil, err
+	}
 	cc := tls.Client(conn, &tls.Config{
 		ServerName:         config.Config.Out.RemoteAddr,
-		ClientSessionCache: tls.NewLRUClientSessionCache(10),
+		ClientSessionCache: tls.NewLRUClientSessionCache(128),
 		MinVersion:         tls.VersionTLS13,
 		MaxVersion:         tls.VersionTLS13,
 	})
 	err = cc.Handshake()
-	return nil, err
+	if nil != err {
+		return nil, err
+	}
+	ec, err := common.NewChacha20Stream([]byte(config.Config.User), cc)
+	if nil != err {
+		return nil, err
+	}
+	var addr = target.String()
+	var l = int16(len(addr))
+	// domain length limit
+	if l > 253 {
+		return nil, errors.New("target address's length large that 253.")
+	}
+	buf := make([]byte, 2)
+	binary.BigEndian.PutUint16(buf, uint16(l))
+	// write domain length
+	_, err = ec.Write(buf)
+	if nil != err {
+		return nil, err
+	}
+	// write domain
+	_, err = ec.Write([]byte(addr))
+	if nil != err {
+		return nil, err
+	}
+
+	return ec, err
 }
