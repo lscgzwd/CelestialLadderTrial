2a0f2343edc2ba7adfcb6f50ae292065ae4d8a60
lushuncheng <i@shuncheng.lu>
2022-11-03 18:33:54 +0800

    tmp

--- server/proxy/http.go
+++ server/proxy/http.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"proxy/server/common"
 	"proxy/utils/context"
@@ -22,8 +24,41 @@ func (s *HttpServer) Start(l net.Listener) {
 func (s *HttpServer) Handshake(ctx *context.Context, conn net.Conn) (io.ReadWriter, *common.TargetAddr, error) {
 	req, _ := ctx.Get("request")
 	request, _ := req.(*http.Request)
-	if request.Method == http.MethodConnect {
 
+	addr := request.Host
+	i := strings.LastIndex(addr, ":")
+	host := addr
+	port := 80
+	var err error
+	if i != -1 {
+		var portStr string
+		host, portStr, err = net.SplitHostPort(addr)
+		if nil != err {
+			return nil, nil, err
+		}
+		port64, err := strconv.ParseInt(portStr, 10, 64)
+		if nil != err {
+			return nil, nil, err
+		}
+		port = int(port64)
 	}
-	return nil, nil, nil
+	if request.Method == http.MethodConnect {
+		_, err := conn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
+		if nil != err {
+			return nil, nil, err
+		}
+		if port == 80 {
+			port = 443
+		}
+	}
+	ip := net.ParseIP(host)
+	var target = &common.TargetAddr{
+		Port: port,
+	}
+	if nil == ip {
+		target.Name = host
+	} else {
+		target.IP = ip
+	}
+	return conn, target, nil
 }
